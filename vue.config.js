9f342f25571eed75084161562090a1dc0c3455c7
Lai chane an <m020203@gms.tku.edu.tw>
2020-05-21 03:56:29 +0800

    fix: icon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  publicPath: './',
+  runtimeCompiler: true,
+  pwa: {
+    workboxOptions: {
+      exclude: [/OneSignal.*\.js$/],
+    },
+  },
   transpileDependencies: [
     'vuetify',
   ],
